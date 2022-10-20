4a2d1c47f26076b79aabe3b231c10cfe64b72aa5
Antonio J. Polo Enriquez <apoloenriquez@deloitte.es>
2022-10-20 20:52:59 +0200

    intall correctly icomoon package, fix icon file extension, added more html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import classes from "./App.module.scss";
+import Icon from "./assets/icomoon/icon.js";
 import logoibercaja from "./assets/images/logo-ibercaja.png";
 import searchicon from "./assets/images/search-icon.svg";
 function App() {
@@ -8,13 +9,24 @@ function App() {
     <div className="App">
       <header className={classes.header}>
         <img className={classes.header_logo} src={logoibercaja}></img>
+
         <nav className={classes.header_nav}>
           <div>PRODUCTOS</div>
           <div>OPERATIVAS</div>
         </nav>
         <div className={classes.search_input}>
-          <img src={searchicon}></img>
-          <input placeholder="componente de busqueda" />
+          <Icon icon="search-icon" size={12} color="white" />
+          <input placeholder="Busca en ibercaja..." />
+        </div>
+        <div className={classes.header_notification}>
+          <div className={classes.header_notification_count}>2</div>
+        </div>
+        <div className={classes.header_user_menu}>
+          img
+          <p>María Salas</p>
+          <span className={classes.header_user_menu_icon}>
+            <Icon icon="arrow_down" size={25} color="white" />
+          </span>
         </div>
       </header>
     </div>
