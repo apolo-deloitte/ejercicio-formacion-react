9537c4150cf8970ceb04f1b640508d7ce6019739
Antonio J. Polo Enriquez <apoloenriquez@deloitte.es>
2022-10-21 14:45:17 +0200

    added avatar user, some html structure and some styles

--- src/App.js
+++ src/App.js
@@ -2,31 +2,44 @@ import { useTranslation } from "react-i18next";
 import classes from "./App.module.scss";
 import Icon from "./assets/icomoon/icon.js";
 import logoibercaja from "./assets/images/logo-ibercaja.png";
-import searchicon from "./assets/images/search-icon.svg";
+import avatar from "./assets/images/avatar.png";
 function App() {
   const { t } = useTranslation();
   return (
     <div className="App">
       <header className={classes.header}>
-        <img className={classes.header_logo} src={logoibercaja}></img>
-
+        <div className={classes.header_logo_bank}>
+          <img className={classes.header_logo} src={logoibercaja}></img>
+        </div>
         <nav className={classes.header_nav}>
           <div>PRODUCTOS</div>
           <div>OPERATIVAS</div>
         </nav>
-        <div className={classes.search_input}>
-          <Icon icon="search-icon" size={12} color="white" />
-          <input placeholder="Busca en ibercaja..." />
-        </div>
-        <div className={classes.header_notification}>
-          <div className={classes.header_notification_count}>2</div>
-        </div>
-        <div className={classes.header_user_menu}>
-          img
-          <p>María Salas</p>
-          <span className={classes.header_user_menu_icon}>
-            <Icon icon="arrow_down" size={25} color="white" />
-          </span>
+        <div className={classes.header_user_container}>
+          <div className={classes.header_user_content}>
+            <div className={classes.search_input}>
+              <Icon
+                icon="search-icon"
+                size={12}
+                color="white"
+                className={classes.icon_search}
+              />
+              <input placeholder="Busca en ibercaja..." />
+            </div>
+            <div className={classes.header_notification_container}>
+              <div className={classes.header_notification_content}>
+                <div className={classes.header_notification_count}>2</div>
+              </div>
+            </div>
+            <div className={classes.header_user_menu}>
+              <img src={avatar} alt="usuario" />
+              <p>María Salas</p>
+              <span className={classes.header_user_menu_icon}>
+                <Icon icon="arrow_down" size={25} color="white" />
+              </span>
+            </div>
+            <div class="App_header_logo_menu__GDGbX">LOGO</div>
+          </div>
         </div>
       </header>
     </div>
